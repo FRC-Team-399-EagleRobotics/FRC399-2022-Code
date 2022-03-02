3db256941135d689c69d7b62cff35a390f6dce8a
Mark Corral <165009@students.avhsd.org>
2022-03-01 22:42:56 -0800

    commit on 'main'

--- FRC399-2022/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ FRC399-2022/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -5,32 +5,15 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.hal.HAL;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.RobotBase;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.Timer;
-
-import javax.lang.model.util.ElementScanner6;
-
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Compressor;
 
 public class ShooterSubsystem extends SubsystemBase {
-  // TODO: instantiate shooter motor controller and hood solenoid here!
 
     /* Joysticks */
     Joystick controller = new Joystick(0); //Controller
@@ -56,14 +39,13 @@ public class ShooterSubsystem extends SubsystemBase {
    */
   public ShooterSubsystem()
   {
-    // TODO: initialize motor controllers and solenoid here!
     if(controller.getRawButton(8))
     {
       vel = 4000;
       shooterSolenoid.set(true);
       ShooterL.set(ControlMode.Velocity, vel);
       ShooterR.set(ControlMode.Velocity, vel);
-      
+
     }else if(controller.getRawButton(7))
     {
       vel = 8500;
@@ -80,7 +62,6 @@ shooterMotors.set(0);
   public void periodic()
   {
     // This method will be called once per scheduler run
-    // TODO: write shooter velocity and hood position values here
     vel= 0;
     pos = false;
   }
