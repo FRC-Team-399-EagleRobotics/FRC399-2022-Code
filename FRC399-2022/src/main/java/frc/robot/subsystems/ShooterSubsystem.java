40fb1fdf1a374b7ccb3ec7eb2e77948a3deb7c13
Mark Corral <165009@students.avhsd.org>
2022-03-03 13:27:11 -0800

    commit on 'main'

--- FRC399-2022/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ FRC399-2022/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -8,7 +8,6 @@ import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
