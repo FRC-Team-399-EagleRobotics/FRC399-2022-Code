a660b4c6a42f19e8fdd5cfce474829d9d6fb6899
Gameboyghost <99833632+DarianR785@users.noreply.github.com>
2022-03-09 13:16:40 -0800

    Importing my backup/modifications

--- FRC399-2022-lastday/src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ FRC399-2022-lastday/src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -4,11 +4,23 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class ClimberSubsystem extends SubsystemBase {
+  // Calling out motors
+  private TalonSRX leftClimberCim1_ID, rightClimberCim1_ID;
+  
   /** Creates a new ExampleSubsystem. */
-  public ClimberSubsystem() {}
+  public ClimberSubsystem() {
+    // IDK if this is the right way got from drivetrain. 
+    // Suppose this is for setting the motor variable 
+    // Idk why its red or where you get init
+    //leftClimberCim1_ID = init(Constants.Climber.leftClimberCim1_ID);
+    //rightClimberCim1_ID = init2(Constants.Climber.leftClimberCim1_ID);
+  }
 
   @Override
   public void periodic() {
