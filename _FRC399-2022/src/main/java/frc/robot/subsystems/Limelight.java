443eb807a969444b989bcf7adf1afedfb949fae2
Jeremy Paul Germita <jeremy.germita@gmail.com>
2022-04-05 15:05:08 -0700

    Create Limelight.java

---
+++ _FRC399-2022/src/main/java/frc/robot/subsystems/Limelight.java
@@ -0,0 +1,34 @@
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+
+public class Limelight {
+
+    NetworkTable table;
+    NetworkTableEntry tx;
+    NetworkTableEntry ty;
+
+    public Limelight() {
+        table = NetworkTableInstance.getDefault().getTable("limelight");
+        tx = table.getEntry("tx");
+        ty = table.getEntry("ty");
+    }
+
+
+    public void setLight(boolean state) {
+        
+        NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(state ? 2 : 1);
+    }
+
+    public double getX() {
+        return tx.getDouble(0.0);
+
+    }
+
+    public double getY() {
+        return ty.getDouble(0.0);        
+    }
+}
